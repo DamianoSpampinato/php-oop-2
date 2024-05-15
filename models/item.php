95dc7235412e29a9646a83dffbcdff4ca3af5699
DamianoSpampinato <ghgg585@gmail.com>
2024-05-15 18:59:15 +0200

    feat add output on screen

---
+++ models/item.php
@@ -0,0 +1,27 @@
+<?php
+//creare entità prodotto
+class Item{
+    //inizializzare tutte le varibiali public per poi modificarle in corso d'opera
+    public $name;
+    public $price;
+    public $category;
+    public $age;
+    public $image;
+    
+    
+    public function __construct($_name, $_price){
+        $this->name = $_name;
+        $this->price = $_price;
+        var_dump($this);
+        
+    }
+    //prendi i dettagli
+    public function getDetails($_category, $_age, $_image){
+        $this->category=$_category;
+        $this->age=$_age;
+        $this->image=$_image;
+    }
+}
+//identificare le classi generiche
+//poi creare entità ereditarie
+?>
